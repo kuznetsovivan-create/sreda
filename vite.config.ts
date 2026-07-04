4af594150b6bd579d22c16ceeca966256ceb0d60
fakoboss <dlyabrawlstars2003@mail.ru>
2026-07-05 00:07:23 +0500

    Обновлен дизайн сайта: добавлены локальные изображения, обновлен логотип и hero секция

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(({ mode }) => {
   return {
-    base: mode === 'production' ? '/remont-kvartir/' : '/',
+    base: mode === 'production' ? '/sreda/' : '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
